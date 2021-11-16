8d6bd7999c84abae6039adce3393b4888d3d0d92
Cwonseok <9617081@gmail.com>
2021-11-16 09:55:54 +0900

    login

--- Odonata/src/main/user/model/dao/UserDAO.java
+++ Odonata/src/main/user/model/dao/UserDAO.java
@@ -1,8 +1,78 @@
 package main.user.model.dao;
 
+import static main.common.JDBCTemplate.close;
+import static main.common.JDBCTemplate.getConnection;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+
+import main.user.model.vo.User;
 
 public class UserDAO {
+
+    private Properties prop = null;
+
+    public UserDAO() {
+        prop = new Properties();
+
+
+        String fileName = UserDAO.class.getResource("/sql/user/user-query.properties").getPath();
+
+        try {
+            prop.load(new FileReader(fileName));
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public User loginUser(String userId, String userPwd, Connection conn) {
+
+
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+        User loginUser = null;
+
+        String query = prop.getProperty("login");
+        
+
+        try {
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, userId);
+            pstmt.setString(2, userPwd);
+
+            rset = pstmt.executeQuery();
+
+            if(rset.next()) {
+                loginUser = new User(rset.getString("User_id"),
+                                        rset.getString("User_pwd"),
+                                        rset.getString("User_name"),
+                                        rset.getString("User_email"),
+                                        rset.getString("User_phone"),
+                                        rset.getDate("User_birth"),
+                                        rset.getString("User_code"),
+                                        rset.getString("User_type"),
+                                        rset.getString("Status"),
+                                        
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally{
+            close(rset);
+            close(pstmt);
+        }
+
+        return loginUser;
+    }
     
     
 }
