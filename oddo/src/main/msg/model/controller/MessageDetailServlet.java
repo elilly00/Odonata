74c90debfe8a4140c6310217230399ec43f1ea28
DESKTOP-RE493AR\Administrator <dornqhdlrnddp200@gmail.com>
2021-11-19 15:23:13 +0900

    메세지 박스 내 세부 출력 부분 추가(초안)

---
+++ oddo/src/main/msg/model/controller/MessageDetailServlet.java
@@ -0,0 +1,57 @@
+package msg.model.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import msg.model.service.MessageService;
+import msg.model.vo.Message;
+
+/**
+ * Servlet implementation class MessageDetailServlet
+ */
+@WebServlet("/msgDetail.ms")
+public class MessageDetailServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MessageDetailServlet() {
+    }
+    
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int mId = Integer.parseInt(request.getParameter("mId"));
+        
+        Message msg = new MessageService().selectMessage(mId);
+        
+        String page = null;
+        if (msg != null) {
+            page = "WEB-INF/views/msg/messageBox.jsp";
+            request.setAttribute("msgDetail", msg);
+        } else {
+            page = "WEB-INF/views/common/errorPage.jsp";
+            request.setAttribute("msg", "쪽지 상세보기 실패");
+        }
+        request.getRequestDispatcher(page).forward(request, response);
+    }
+    
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+    
+}
---
+++ oddo/src/main/msg/model/service/MessageService.java
@@ -0,0 +1,46 @@
+package msg.model.service;
+
+import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import msg.model.dao.MessageDAO;
+import msg.model.vo.Message;
+import msg.model.vo.PageInfo;
+
+public class MessageService {
+    
+    private MessageDAO mDAO = new MessageDAO();
+    
+    public int getListCount(String userId) {
+        Connection conn = getConnection();
+        
+        int listCount = mDAO.getListCount(conn, userId);
+        
+        close(conn);
+        
+        return listCount;
+    }
+    
+    public ArrayList<Message> selectList(String userId, PageInfo pi) {
+        Connection conn = getConnection();
+        
+        ArrayList<Message> list = mDAO.selectList(conn, userId, pi);
+        
+        close(conn);
+        
+        return list;
+    }
+    
+    public Message selectMessage(int mId) {
+        Connection conn = getConnection();
+        
+        Message msg = mDAO.selectMessage(conn, mId);
+        
+        close(conn);
+        
+        return msg;
+    }
+}
