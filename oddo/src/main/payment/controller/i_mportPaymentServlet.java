6d89add9c1197c7ff0e62977b67db309cde77620
duiii_iii <alswj792000@naver.com>
2021-11-19 17:16:35 +0900

    i_mportPaymentServlet.java commit

--- oddo/src/main/payment/controller/i_mportPaymentServlet.java
+++ oddo/src/main/payment/controller/i_mportPaymentServlet.java
@@ -10,14 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class paymentForm
  */
-@WebServlet("/reservationPayment.do") //url다른거 없음 근데 왜 연결안돼냐고ㅠㅠ
-public class paymentFormServlet extends HttpServlet {
+@WebServlet("/reservationPayment.do") 
+public class i_mportPaymentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public paymentFormServlet() {
+    public i_mportPaymentServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,17 +28,9 @@ public class paymentFormServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		
-		String bName = request.getParameter("name"); // 주문자 이름
-		int bTel = Integer.parseInt(request.getParameter("tel")); // 주문자 전화번호
-		String bEmail = request.getParameter("email"); // 주문자 이메일
-		int amount = Integer.parseInt(request.getParameter("")); // 숙소 가격 받아오기
-		String rName = request.getParameter("");  // 숙소이름
-		// 주문번호 따로 입력? 
+		// 아무것도 입력하지 않아도 결제 됌 but 서블릿이 없으면 에러나므로 그냥 빈창으로 두겠습니다.
 		
-		
-		
-		// i_mPort로 이동 : 가져갈 데이터가  필요하므로  forward 이용
-		request.getRequestDispatcher("view/i_mPort.jsp").forward(request, response);;
+//		request.getRequestDispatcher("view/payment_finished.jsp").forward(request, response);;
 	}
 
 	/**
