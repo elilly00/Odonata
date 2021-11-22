408b1b0d3684a94a9921bb457f5d105eec6bcdbc
Cwonseok <9617081@gmail.com>
2021-11-22 11:22:04 +0900

    아이디 찾기 비밀번호 찾기 (쿼리 수정 필요 , form 에 뿌려주는지 확인 못함 , Test 못함,,

--- oddo/src/main/user/model/service/UserService.java
+++ oddo/src/main/user/model/service/UserService.java
@@ -4,69 +4,82 @@ import static main.common.JDBCTemplate.close;
 import static main.common.JDBCTemplate.getConnection;
 
 import java.sql.Connection;
-
 import main.user.model.dao.UserDAO;
 import main.user.model.vo.User;
 
 public class UserService {
 
-    private UserDAO uDAO = new UserDAO();
+  private UserDAO uDAO = new UserDAO();
 
-    public User loginUser(String userId, String userPwd) {
+  public User loginUser(String userId, String userPwd) {
+    Connection conn = getConnection();
 
-        Connection conn = getConnection();
+    User loginUser = uDAO.loginUser(userId, userPwd, conn);
 
-        User loginUser = uDAO.loginUser(userId, userPwd, conn);
+    close(conn);
 
+    return loginUser;
+  }
 
-        close(conn);
+  public int insertUser(User u) {
+    Connection conn = getConnection();
 
-        return loginUser;
-    }
-    
-    public int insertUser(User u) {
-        Connection conn = getConnection();
-        
-        int result = uDAO.insertUser(conn, u);
-        
-        if (result > 0)
-            commit(conn);
-        else
-            rollback(conn);
-        
-        close(conn);
-        
-        return result;
-    }
-    
-    public int checkId(String inputId) {
-        Connection conn = getConnection();
-        
-        int result = uDAO.checkId(conn, inputId);
-        
-        close(conn);
-        
-        return result;
-    }
-    
-    public int checkEmail(String inputEmail) {
-        Connection conn = getConnection();
-        
-        int result = uDAO.checkEmail(conn, inputEmail);
-        
-        close(conn);
-        
-        return result;
-    }
-    
-    public User selectUser(String userId) {
-        Connection conn = getConnection();
-        
-        User user = uDAO.selectUser(conn, userId);
-        
-        close(conn);
-        
-        return user;
-    }
-    
+    int result = uDAO.insertUser(conn, u);
+
+    if (result > 0) commit(conn); else rollback(conn);
+
+    close(conn);
+
+    return result;
+  }
+
+  public int checkId(String inputId) {
+    Connection conn = getConnection();
+
+    int result = uDAO.checkId(conn, inputId);
+
+    close(conn);
+
+    return result;
+  }
+
+  public int checkEmail(String inputEmail) {
+    Connection conn = getConnection();
+
+    int result = uDAO.checkEmail(conn, inputEmail);
+
+    close(conn);
+
+    return result;
+  }
+
+  public User selectUser(String userId) {
+    Connection conn = getConnection();
+
+    User user = uDAO.selectUser(conn, userId);
+
+    close(conn);
+
+    return user;
+  }
+
+public User idfind(String name, String email) {
+    Connection conn = getConnection();
+
+    User user = uDAO.idfind(conn, name, email);
+
+    close(conn);
+
+    return user;
+}
+
+public User idfind(String id, String name, String email) {
+  Connection conn = getConnection();
+
+  User user = uDAO.pwdfind(conn, id, name, email);
+
+  close(conn);
+
+  return user;
+}
 }
