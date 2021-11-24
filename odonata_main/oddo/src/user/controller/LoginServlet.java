d0579d63609e13cd4f28a607fcfbe2202140fea8
Cwonseok <9617081@gmail.com>
2021-11-24 15:27:27 +0900

    loginservlet userdao import 추가

--- odonata_main/oddo/src/user/controller/LoginServlet.java
+++ odonata_main/oddo/src/user/controller/LoginServlet.java
@@ -1,6 +1,7 @@
 package user.controller;
 
 import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import user.model.service.UserService;
 import user.model.vo.User;
 
--- odonata_main/oddo/src/user/model/dao/UserDAO.java
+++ odonata_main/oddo/src/user/model/dao/UserDAO.java
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
+
 import user.model.vo.User;
 
 public class UserDAO {
