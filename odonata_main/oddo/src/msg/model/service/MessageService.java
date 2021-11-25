41a86a5b84af955c77db2908b2615434cd0fab67
Cwonseok <9617081@gmail.com>
2021-11-25 22:25:30 +0900

    음 정리 취소

--- odonata_main/oddo/src/msg/model/service/MessageService.java
+++ odonata_main/oddo/src/msg/model/service/MessageService.java
@@ -5,58 +5,57 @@ import static common.JDBCTemplate.getConnection;
 
 import java.sql.Connection;
 import java.util.ArrayList;
-
 import msg.model.dao.MessageDAO;
 import msg.model.vo.Message;
 import msg.model.vo.PageInfo;
 
 public class MessageService {
-    
-    private MessageDAO mDAO = new MessageDAO();
-    
-    public int getListCount(String userId) {
-        Connection conn = getConnection();
-        
-        int listCount = mDAO.getListCount(conn, userId);
-        
-        close(conn);
-        
-        return listCount;
-    }
-    
-    public ArrayList<Message> selectList(String userId, PageInfo pi) {
-        Connection conn = getConnection();
-        
-        ArrayList<Message> list = mDAO.selectList(conn, userId, pi);
-        
-        close(conn);
-        
-        return list;
-    }
-    
-    public Message selectMessage(int mId) {
-        Connection conn = getConnection();
-        
-        Message msg = mDAO.selectMessage(conn, mId);
-        
-        close(conn);
-        
-        return msg;
+
+  private MessageDAO mDAO = new MessageDAO();
+
+  public int getListCount(String userId) {
+    Connection conn = getConnection();
+
+    int listCount = mDAO.getListCount(conn, userId);
+
+    close(conn);
+
+    return listCount;
+  }
+
+  public ArrayList<Message> selectList(String userId, PageInfo pi) {
+    Connection conn = getConnection();
+
+    ArrayList<Message> list = mDAO.selectList(conn, userId, pi);
+
+    close(conn);
+
+    return list;
+  }
+
+  public Message selectMessage(int mId) {
+    Connection conn = getConnection();
+
+    Message msg = mDAO.selectMessage(conn, mId);
+
+    close(conn);
+
+    return msg;
+  }
+
+  public int insertMessage(Message msg) {
+    Connection conn = getConnection();
+
+    int result = mDAO.insertMessage(conn, msg);
+
+    if (result > 0) {
+      commit(conn);
+    } else {
+      rollback(conn);
     }
 
-    public int insertMessage(Message msg) {
-		Connection conn = getConnection();
-		
-		int result = mDAO.insertMessage(conn, msg);
-		
-		if(result > 0) {
-			commit(conn);
-		} else {
-			rollback(conn);
-		}
-		
-		close(conn);
-		
-		return result;
-	}
+    close(conn);
+
+    return result;
+  }
 }
