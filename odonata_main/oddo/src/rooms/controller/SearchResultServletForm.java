14be3b3babee59d0fade3d86eb401fada4cc359b
Cwonseok <9617081@gmail.com>
2021-11-26 09:28:22 +0900

    index 검색

--- odonata_main/oddo/src/rooms/controller/SearchResultServletForm.java
+++ odonata_main/oddo/src/rooms/controller/SearchResultServletForm.java
@@ -35,50 +35,9 @@ public class SearchResultServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // int listCount;
-        // int currentPage;
-        // int pageLimit;
-        // int boardLimit;
-        // int maxPage;
-        // int startPage;
-        // int endPage;
-        
-        RoomsService rService = new RoomsService();
-        
-        // listCount = rService.getListCount(); // �� �Խñ� ���� 
-        
-        // currentPage = 1;
-        // if (request.getParameter("currentPage") != null) {
-        //     currentPage = Integer.parseInt(request.getParameter("currentPage"));
-        // }
-        
-        // pageLimit = 5;
-        // boardLimit = 4;
-        
-        // maxPage = (int) Math.ceil((double) listCount / boardLimit);
-        // startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
-        // endPage = startPage + (pageLimit - 1);
-        // if (maxPage < endPage) {
-        //     endPage = maxPage;
-        // }
-        
-        // PageInfo pi = new PageInfo(currentPage, listCount, pageLimit, boardLimit, maxPage, startPage, endPage);
-        
-        ArrayList<Rooms> rList = rService.selectTList(1); 		// 숙소
-        ArrayList<sooksoImg> ImgList = rService.selectTList(2);   // 숙소 이미지
-        
-        String page = null;
-        if (rList != null && ImgList != null) {
-            request.setAttribute("rList", rList);
-            request.setAttribute("ImgListfList", ImgList);
-            // request.setAttribute("pi", pi);
-            page = "WEB-INF/view/search_result.jsp";
-        } else {
-            request.setAttribute("msg", "검색 조회 실패");
-            page = "WEB-INF/view/errorPage.jsp";
-        }
-        
-        request.getRequestDispatcher(page).forward(request, response);
+                request
+                .getRequestDispatcher("WEB-INF/view/rooms/Search_result.jsp")
+                .forward(request, response);
     }
     
     /**
